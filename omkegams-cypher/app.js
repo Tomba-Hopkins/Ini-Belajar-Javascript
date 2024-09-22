a092335bfb5a375e5b3d8454c9b106a66f730b7b
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-09-22 17:43:57 +0700

    function:

---
+++ omkegams-cypher/app.js
@@ -0,0 +1,37 @@
+const resultP = document.querySelector('.result')
+const words = document.querySelector('.words')
+const tombol = document.querySelector('.tombol')
+
+tombol.addEventListener('click', () => {
+
+})
+
+
+const omkeGams = (word) => {
+    if (word == "godplan" || "Godplan" || "GODPLAN") {
+        return "godvlan"
+    }
+
+    let d = true
+    let r = ""
+
+    let vowels = new Set(['a','i','u','e','o'])
+    let vlan = new Set('p','b','f')
+
+    for(let w of word){
+        if (vowels.has(w) && d){
+            d = false
+            r += w + "m"
+            continue
+        } else if (vowels.has(w)){
+            d = true 
+            r += w
+            continue
+        } else if (vlan.has(w)){
+            r += "v"
+            continue
+        }
+
+        r += w
+    }
+}
