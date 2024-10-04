06a7e05fe837469656235522ba1c58032642619f
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-10-04 08:31:19 +0700

    done: roman numeral convert

--- freecodecamp/roman-numeral-convert/script.js
+++ freecodecamp/roman-numeral-convert/script.js
@@ -10,13 +10,26 @@ btn.addEventListener('click', () => {
         res.textContent = "Please enter a valid number"
     } else if (numVal > 3999) {
         res.textContent = "Please enter a number less than or equal to 3999"
+    } else {
+        res.textContent = convRoman(numVal)
     }
+
+
     
 })
 
 const convRoman = (number) => {
-    let numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    let keyNums = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
     let romans = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
     let result = ""
+
+    for(let i = 0; i < keyNums.length; i++) {
+        while(number >= keyNums[i]) {
+            number -= keyNums[i]
+            result += romans[i]
+        }
+    }
+
+    return result
 
 }
